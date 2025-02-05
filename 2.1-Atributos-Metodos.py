92339b7f45763f5cd75990ccddf2ec5d73972b8a
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-05 19:32:41 -0300

    Feat: Aplicação de sobre atributos

--- 2.1-Atributos-Metodos.py
+++ 2.1-Atributos-Metodos.py
@@ -1,3 +1,44 @@
-# Continuacao
+# Continuacao Aula 02
 # Atributos e Metodos
+
+# Teoria sobre Atributos
+
+"""
+Tipos de Atributos:
+- Atributos de Instância (mais comuns)
+-- São específicos de cada objeto.
+--Definidos dentro do método __init__().
+
+-Atributos de Classe
+--Compartilhados entre todas as instâncias de uma classe.
+--Definidos diretamente dentro da classe (fora de __init__()).
+
+Vide exeplo do arquivo 02-Denifinica-classe
+"""
+# Exemplo de Atributos de Classe
+
+class Pessoa:
+    especie = "Humano"  # Atributo de classe (compartilhado)
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+# Criando objetos
+pessoa1 = Pessoa("João", 30)
+pessoa2 = Pessoa("Maria", 25)
+
+print(pessoa1.especie)  # Saída: Humano
+print(pessoa2.especie)  # Saída: Humano
+
+# Alterando o atributo de classe
+Pessoa.especie = "Ser Vivo"
+
+print(pessoa1.especie)  # Saída: Ser Vivo
+print(pessoa2.especie)  # Saída: Ser Vivo
+
+
+
+
+
 
