c7e6650438d34ef8322235bce58276c55d84b7e2
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-09 13:37:46 -0300

    feat: adicionando criar_conta

--- Orientacao-Objeto/4.3-exercico-final-banco-interativo.py
+++ Orientacao-Objeto/4.3-exercico-final-banco-interativo.py
@@ -1,9 +1,23 @@
 class ContaBancaria:
     
     contas = {}
+    proxima_conta = 1000
     
-def __init__(self, titular, conta, saldo):
-    self.titular = titular
-    self.conta = conta
-    self.saldo = saldo
-    ContaBancaria.contas[conta] = titular
+    def __init__(self, titular, conta, saldo):
+        self.titular = titular
+        self.conta = conta
+        self.saldo = saldo
+        ContaBancaria.contas[conta] = titular
+    
+class CriarConta:
+    def __init__(self):
+        self.conta_bancaria = ContaBancaria(None, None, None)
+
+    def criar_conta(self):
+        titular = input("Digite o nome do titular: ")
+        conta = ContaBancaria.proxima_conta+len(ContaBancaria.contas)
+        self.conta_bancaria.titular = titular
+        self.conta_bancaria.conta = conta
+        ContaBancaria.contas[conta] = titular
+        ContaBancaria.proxima_conta += 1
+        return conta
