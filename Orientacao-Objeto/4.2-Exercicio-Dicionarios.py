befc8cebdb9c6b04be729cd64cc6a972cca757e9
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-09 12:31:18 -0300

    chore: imprimir corrigir_nota

--- Orientacao-Objeto/4.2-Exercicio-Dicionarios.py
+++ Orientacao-Objeto/4.2-Exercicio-Dicionarios.py
@@ -20,6 +20,13 @@ def calcular_media():
         print(f"\n Média da turma: {media:.2f}")
     else:
         print("\n Nenhum aluno cadastrado.")
+
+def corrigir_nota(nome,nota_nova):
+    if nome in alunos:
+        alunos[nome] = nota_nova
+        print(f"Nota de {nome} corrigida para {nota_nova}.")
+    else:
+        print(f"Aluno {nome} nao encontrado.")
         
 # Teste do sistema
 adicionar_aluno("Ana", 8.5)
@@ -27,4 +34,6 @@ adicionar_aluno("Carlos", 7.0)
 adicionar_aluno("Bianca", 9.2)
 
 mostrar_notas()
+calcular_media()
+corrigir_nota("Ana", 9.0)
 calcular_media()
