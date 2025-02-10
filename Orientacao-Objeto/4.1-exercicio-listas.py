1c590e86a39ba847c8c6cf9c73b7d50911660474
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-10 18:45:24 -0300

    feat: adicionada nova funca editar_tarefas

--- Orientacao-Objeto/4.1-exercicio-listas.py
+++ Orientacao-Objeto/4.1-exercicio-listas.py
@@ -1,6 +1,14 @@
 # 1. Exercício com Listas
 #  Objetivo: Criar um sistema de gerenciamento de tarefas usando listas.
 
+"""
+Pratique:
+
+Adicione mais tarefas.
+Tente remover uma tarefa inexistente.
+Melhore o código, adicionando uma opção para editar tarefas.
+"""
+
 # Lista para armazenar tarefas
 
 tarefas = []
@@ -14,12 +22,20 @@ def remover_tarefa(tarefa):
         tarefas.remove(tarefa)
         print(f"Tarefa '{tarefa}' removida!")
     else:
-        print("Tarefa nao encontrada!")
+        print(f"Tarefa '{tarefa}' nao encontrada!")
         
 def listar_tarefas():
     print("\n Lista de Tarefas:")
     for i, tarefa in enumerate(tarefas, 1):
         print(f"{i}. {tarefa}")
+        
+def editar_tarefa(tarefa, nova_tarefa):
+    if tarefa in tarefas:
+        index = tarefas.index(tarefa)
+        tarefas[index] = nova_tarefa
+        print(f"Tarefa '{tarefa}' editada para '{nova_tarefa}'!")
+    else:
+        print(f"Tarefa '{tarefa}' nao encontrada!")
         
         # Teste do sistema
 adicionar_tarefa("Estudar Python")
@@ -30,3 +46,21 @@ listar_tarefas()
 
 remover_tarefa("Estudar Python")
 listar_tarefas()
+
+adicionar_tarefa("Atualizar Wakatime")
+adicionar_tarefa("Fazer commit dos exercicios")
+
+listar_tarefas()
+
+remover_tarefa("Atualizar Wakatime")
+remover_tarefa("Estudar Python")
+
+listar_tarefas()
+
+editar_tarefa("Fazer exercícios de POO", "Fazer Praticas de POO")
+
+listar_tarefas()
+
+
+
+
