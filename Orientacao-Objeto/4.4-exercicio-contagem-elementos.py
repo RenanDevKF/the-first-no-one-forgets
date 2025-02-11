15bcc47f71bb6085c308396eee428f5ab45bbc2d
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-11 05:39:12 -0300

    docs: 'pratique' para o inicio

--- Orientacao-Objeto/4.4-exercicio-contagem-elementos.py
+++ Orientacao-Objeto/4.4-exercicio-contagem-elementos.py
@@ -1,14 +1,4 @@
 #4. Exercício com Counter (Contagem de elementos)
-# Objetivo: Contar quantas vezes cada palavra aparece em uma lista usando Counter
-
-from collections import Counter
-
-palavras = ["Python", "Java", "Python", "C++", "Java", "Python", "C#"]
-contagem = Counter(palavras)
-
-print("\n📊 Frequência das palavras:")
-for palavra, qtd in contagem.items():
-    print(f"{palavra}: {qtd}")
 
 """ 
 Pratique:
@@ -17,3 +7,15 @@ Teste com uma lista maior.
 Modifique o código para contar letras em uma frase.
 
 """
+# Objetivo: Contar quantas vezes cada palavra aparece em uma lista usando Counter
+
+from collections import Counter
+
+palavras = ["Python", "Java", "Python", "C++", "Java", "Python", "C#"]
+contagem = Counter(palavras)
+
+
+print("\n📊 Frequência das palavras:")
+for palavra, qtd in contagem.items():
+    print(f"{palavra}: {qtd}")
+
