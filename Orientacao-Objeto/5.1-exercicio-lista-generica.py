20265ab377b56455fb8a0f15cbf862ef1a98ebea
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-11 18:14:47 -0300

    feat: exercicio pratico lista generica

---
+++ Orientacao-Objeto/5.1-exercicio-lista-generica.py
@@ -0,0 +1,39 @@
+"""Criando uma Lista Genérica
+Crie uma classe ListaGenerica que pode armazenar uma lista de qualquer tipo de dado. Implemente os seguintes métodos:
+
+adicionar(item: T): Adiciona um item à lista.
+remover() -> T: Remove e retorna o último item da lista.
+mostrar_lista(): Exibe todos os itens da lista.
+💡 Teste armazenando números, strings e objetos personalizados.
+"""
+
+from typing import Generic, TypeVar
+
+T = TypeVar ('T')   # Criando um tipo genérico
+
+class ListaGenerica(Generic[T]):
+    
+    def __init__(self):
+        self.item = []
+    
+    def adicionar(self, item: T):
+        self.item.append(item)
+    
+    def remover(self):
+        if self.item:
+            return self.item.pop()
+        else:
+            return None
+    
+    def mostrar_lista(self) -> T:
+        return self.item.copy() # Retorna uma cópia da lista para evitar alterações externas
+    
+lista = ListaGenerica() # Criando uma instância da classe
+    
+lista.adicionar(10)
+lista.adicionar("Renan")
+lista.adicionar([1, 2, 3])
+print(lista.mostrar_lista())
+
+print(lista.remover())
+print(lista.mostrar_lista())
