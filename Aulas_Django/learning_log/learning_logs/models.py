35d6183c268e87ee1f8a6d139a7f10169900e108
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-24 07:05:37 -0300

    feat: criar tabela no banco de dados

--- Aulas_Django/learning_log/learning_logs/models.py
+++ Aulas_Django/learning_log/learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """
+    Um assunto sobre o qual o usuario está aprendendo
+    """
+    text = models.CharField(max_length=200)
+    date_added = models.DateField(auto_now_add=True)
+    
+
+    def __str__(self):
+        """Devolve uma representação em string no modelo."""
+        return self.text
