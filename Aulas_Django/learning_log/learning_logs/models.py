32b430bef59ab959a72fb401b8915b8cdbd62d4c
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-07 20:02:35 -0300

    fix: correção de rro DataTimeFIled

--- Aulas_Django/learning_log/learning_logs/models.py
+++ Aulas_Django/learning_log/learning_logs/models.py
@@ -5,7 +5,7 @@ class Topic(models.Model):
     Um assunto sobre o qual o usuario está aprendendo
     """
     text = models.CharField(max_length=200)
-    date_added = models.DateField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
     
 
     def __str__(self):
@@ -26,3 +26,7 @@ class Entry(models.Model):
         """Devolve uma representação em string no modelo."""
         return f"{self.text[:50]}..."
     
+ 
+ 
+ 
+ 
